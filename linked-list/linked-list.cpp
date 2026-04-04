0a3634899429dd6a581cd506eb0ee2724ee2a3e9
Sajid Habib <sajidchughtai779@gmail.com>
2026-04-04 15:12:16 +0500

    linked list completed

--- linked-list/linked-list.cpp
+++ linked-list/linked-list.cpp
@@ -86,6 +86,35 @@ public:
     tail = temp;
   }
 
+  void insert(int val, int pos)
+  {
+    if (pos < 0)
+    {
+      cout << "invalid posation\n";
+      return;
+    }
+
+    if (pos == 0)
+    {
+      push_front(val);
+      return;
+    }
+
+    Node *temp = head;
+    for (int i = 0; i < pos - 1; i++)
+    {
+      if (temp == NULL)
+      {
+        cout << "invalid position\n";
+        return;
+      }
+
+      temp = temp->next;
+    }
+    Node *newNode = new Node(val);
+    newNode->next = temp->next;
+    temp->next = newNode;
+  };
   void printLL()
   {
     Node *temp = head;
@@ -96,24 +125,35 @@ public:
     }
     cout << "NULL" << endl;
   }
+
+  int search(int key)
+  {
+    Node *temp = head;
+    int idx = 0;
+    while (temp != NULL)
+    {
+      if (temp->data == key)
+      {
+        return idx;
+      }
+      temp = temp->next;
+      idx++;
+    }
+    return -1;
+  }
 };
 
 int main()
 {
   List ll;
-  ll.push_front(1);
-  ll.push_front(2);
   ll.push_front(3);
+  ll.push_front(2);
+  ll.push_front(1);
 
-  ll.push_back(4);
-
-  ll.pop_front();
-
-  ll.printLL();
-
-  ll.pop_back();
+  ll.insert(4, 0);
 
   ll.printLL();
+  cout << ll.search(3) << endl;
 
   return 0;
 }
